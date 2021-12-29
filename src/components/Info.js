bed2842b95d171f97700401b125b1a34ccb3afd7
Hana Čopáková <sandracopakova@gmail.com>
2021-12-29 21:41:33 +0100

    add btn links

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,21 +4,21 @@ import InfoFoto from "../images/foto.jpg";
 export default function Info() {
   return (
     <header className="info">
-      <img src={InfoFoto} className="info--foto" />
+      <img src={InfoFoto} className="info--foto" title="Do you wanna hire me? 🥺" alt="smiling Háňa in natural habitat" />
       <h1 className="info--heading">Hana Čopáková</h1>
       <h2 className="info--subheading">Frontend Developer</h2>
       <a href="https://bit.ly/317UyEQ" className="info--portfolio">
         portfolio
       </a>
-      <div className="info-buttons">
-        <button className="info-buttons__btn">
+      <div className="info-buttons info-buttons__btn--email">
+        <a href="mailto: sandracopakova@gmail.com" className="info-buttons__btn">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" fill="none" viewBox="0 0 17 16">
             <path fill="#1E1F26" d="M2.102 4.707L8.5 7.906l6.398-3.199A1.6 1.6 0 0013.3 3.2H3.7a1.6 1.6 0 00-1.598 1.507z"></path>
             <path fill="#1E1F26" d="M14.9 6.494l-6.4 3.2-6.4-3.2V11.2a1.6 1.6 0 001.6 1.6h9.6a1.6 1.6 0 001.6-1.6V6.494z"></path>
           </svg>
           Email
-        </button>
-        <button className="info-buttons__btn info-buttons__btn--linkedin">
+        </a>
+        <a href="https://www.linkedin.com/in/hana-copakova/" className="info-buttons__btn info-buttons__btn--linkedin">
           {" "}
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
             <path
@@ -27,7 +27,7 @@ export default function Info() {
             ></path>
           </svg>
           LinkedIn
-        </button>
+        </a>
       </div>
     </header>
   );
