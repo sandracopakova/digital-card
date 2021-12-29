35c5a641d634b8197e9c976809891069d3f3ffd1
Hana Čopáková <sandracopakova@gmail.com>
2021-12-29 15:27:42 +0100

    add About component

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Interests() {
   return (
-    <>
+    <section>
       <h2>Interests</h2>
       <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
-    </>
+    </section>
   );
 }
