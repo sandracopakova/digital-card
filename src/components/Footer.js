9031b47230339d91a7b32888e24c60dc3cac98d6
Hana Čopáková <sandracopakova@gmail.com>
2021-12-31 11:51:49 +0100

    fix names of images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Twitter from "../images/Twitter.png";
-import Facebook from "../images/Facebook.png";
-import Instagram from "../images/Instagram.png";
-import GitHub from "../images/GitHub.png";
+import github from "../images/github.png";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <div className="footer-info">sandracopakova@gmail.com</div>
       <a href="https://github.com/sandracopakova">
-        <img src={GitHub} alt="twitter icon" />
+        <img src={github} alt="github icon" />
       </a>
     </footer>
   );
 }
-
-/*<img src={Twitter} alt="twitter icon"/>
-      <img src={Facebook} alt="twitter icon"/>
-      <img src={Instagram} alt="twitter icon"/>*/
