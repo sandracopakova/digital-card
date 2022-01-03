27ebbf149e4c3da58a4fcc43a752d34bdda443dd
Hana Čopáková <sandracopakova@gmail.com>
2022-01-03 12:40:40 +0100

    refactor headings

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section>
-      <h3>About</h3>
+      <h2 className="about-heading">About</h2>
       <p>I am a junior frontend developer and am always looking for new things to learn. Now I'm interested in React.</p>
     </section>
   );
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Interests() {
   return (
     <section>
-      <h3>Interests</h3>
+      <h2 className="interests-heading">Interests</h2>
       <p>Sport fanatic. Plant scholar. Ice cream buff. Travel geek.</p>
     </section>
   );
