3feff9ab2d3332cac2778d0a5b009e4a46bec395
Hana Čopáková <hancacopakova@gmail.com>
2022-08-26 14:37:45 +0200

    fix(app): update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import github from "../images/github.png";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__info">sandracopakova@gmail.com</div>
-      <a href="https://github.com/sandracopakova">
+      <div className="footer__info">hancacopakova@gmail.com</div>
+      <a href="https://github.com/violinie">
         <img src={github} alt="Hana's GitHub" />
       </a>
     </footer>
